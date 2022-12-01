c9b635ed1ee3c5154eddc2923a0da42ba3c42d48
Kyle Lee <kyleleeca@gmail.com>
2022-12-01 12:01:53 -0500

    add signup page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 import Home from "./pages/Home";
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
         <Footer />
       </BrowserRouter>
---
+++ src/pages/Signup.jsx
@@ -0,0 +1,100 @@
+import { useState } from "react";
+import randomImage from "../random-img.jpg";
+import "../styles/form.css";
+
+const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [password, setPassword] = useState("");
+  const [password2, setPassword2] = useState("");
+
+  const handleSubmit = () => {
+    alert("submit request");
+  };
+
+  return (
+    <div className="form-container container">
+      <form onSubmit={handleSubmit}>
+        <h1 className="form-heading">Sign Up</h1>
+        <div className="form-item">
+          <p>
+            <label for="email">Email</label>
+          </p>
+          <input
+            type="text"
+            name="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="form-items">
+          <div className="form-item">
+            <p>
+              <label for="firstName">First Name</label>
+            </p>
+            <input
+              type="text"
+              name="firstName"
+              id="firstName"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+          </div>
+          <div className="form-item">
+            <p>
+              <label for="lastName">Last Name</label>
+            </p>
+            <input
+              type="text"
+              name="lastName"
+              id="lastName"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </div>
+        </div>
+
+        <div className="form-item">
+          <p>
+            <label htmlFor="password">Password</label>
+          </p>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="form-item">
+          <p>
+            <label htmlFor="password2">Confirm Password</label>
+          </p>
+          <input
+            type="password"
+            name="password2"
+            id="password2"
+            value={password2}
+            onChange={(e) => setPassword2(e.target.value)}
+          />
+        </div>
+        <div className="form-item">
+          <p>
+            <a href="#" className="form-link">
+              Already have an account with us?
+            </a>
+          </p>
+          <button type="submit" className="form-btn">
+            Login
+          </button>
+        </div>
+      </form>
+      <div className="form-image">
+        <img src={randomImage} alt="toronto-img"></img>
+      </div>
+    </div>
+  );
+};
+export default Signup;
