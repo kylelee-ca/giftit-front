736275ccc322dd845f948c384e3a577fd93a6fd8
Kyle Lee <kyleleeca@gmail.com>
2022-12-06 00:32:24 -0500

    add general sidenav

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,22 @@
+import "../styles/sidebar.css";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar-container">
+      <div className="sidebar-category-title">Categories</div>
+      <ul>
+        <li className="sidebar-category-item">New Arrival</li>
+        <li className="sidebar-category-item">natus ab mollitia</li>
+        <li className="sidebar-category-item">Lorem ipsum</li>
+        <li className="sidebar-category-item">dolor sit</li>
+        <li className="sidebar-category-item">amet consectetur</li>
+        <li className="sidebar-category-item">Quae at </li>
+        <li className="sidebar-category-item">Quae at quod</li>
+        <li className="sidebar-category-item">maxime assumenda</li>
+        <li className="sidebar-category-item">veritatis</li>
+        <li className="sidebar-category-item">libero</li>
+      </ul>
+    </div>
+  );
+};
+export default Sidebar;
